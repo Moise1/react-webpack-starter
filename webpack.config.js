e01b4173dac1c154f6db79e28fce2d5a626815b5
Moise Rwibutso <eric.rwibutso@EricRwibutsos-MacBook-Pro.local>
2020-01-16 17:22:45 +0200

    rendering a component using Webpack and Babel from scratch

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js', 
     output: {
-        path: path.join(__dir, '/dist'),
+        path: path.join(__dirname, '/dist'),
         filename: 'index_bundle.js'
     }, 
 
     module:  {
         rules: [
             {
-                test
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
             }
         ]
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 }
